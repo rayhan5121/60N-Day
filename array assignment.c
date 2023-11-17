4b3052034e9ad8c12672d187cbb9326e167b7ecd
rayhan5121 <150989745+rayhan5121@users.noreply.github.com>
2023-11-17 18:19:49 +0600

    Add files via upload

---
+++ array assignment.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+main( )
+{
+      float  a[20];  int  sum=0,   i  ,  n; 
+      
+      printf("Insert number of course");
+      scanf("%d" , &n);
+      
+      for (i=1 ;  i<=n ;  i++)
+      {
+            printf("Enter the GPA of course  %d :  " ,   i);
+            scanf("%f" , &a[i]);
+            
+      }
+      for  (i=1;   i<=n;   i++)
+      {
+            sum =    sum  +   a[i];
+            
+            
+      
+      }
+      printf("\nAverage Cgpa is :  %.2f", sum/( float)n);
+}
